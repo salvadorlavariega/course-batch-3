bb0df65086e9357ee6b17cf77313cc0539eddbde
Slavariega <salvador.lav@gmail.com>
2018-07-20 12:21:48 -0500

    responsive table with css

--- salvadorlavariega/responsive-table/responsive-table.js
+++ salvadorlavariega/responsive-table/responsive-table.js
@@ -18,104 +18,177 @@ class ResponsiveTable extends PolymerElement {
             display: flex;
             width: 100%;
         }
-
+        
         div a{
             display: block;
-            background: darkorange;
-            color: #fff;
+            color: #575757;
             padding: 1rem;
             margin: 0.3rem;
             text-decoration: none;
+            border-bottom: solid;
+            border-width: 1.3px;
+            padding-bottom: 5px;
+            text-align: center;
         }
-        ul{
-            text-decoration: none;
-            list-style: none;
+        .row{
+            padding: 0px;
         }
+        .cell{
+            padding-left: 1rem;
+        }
+        table{
+        width: 100%;
+        }
+        table tr.full-screen:nth-child(even){
+            background: #e0e0e0
+        }
+        table tr.full-screen:nth-child(odd) {
+            background: #FFF
+        }
+        
+        .row_active{
+            color: #575757;
+        }
+        
 
         @media (max-width:530px) {
+            div a{
+                text-align: left;
+            }
             #container {
                 flex-direction: column;
             }
             span{
-                font-weight: bold;
+                color: #3b93ca;
+                width: 87px;
             }
-            ul{
+            .row{
                 display: none;
+                padding-left: 1rem;
             }
-            span{
-                font-weight: bold;
+            .row_active{
+                color: #3b93ca;
             }
+            
         }
+        
+  
       </style>
       <div id="container">
             <template is="dom-repeat" items ="[[grid]]">
               <div>
                   <a href="#" on-click="displayNext">[[item.header]]</a>
-                  <ul>
-                      <template is="dom-repeat" items="[[item.cells]]" as="cell">
-                        <template is="dom-if" if="[[!mode_movil]]">
-                            <li>[[cell]]</li>
-                        </template>  
-                        <template is="dom-if" if="[[mode_movil]]">
-                            <li><span>[[cell.label]]: </span> {{cell.value}}</li>
+                  <div class="row">
+                    
+                                        
+                        <template is="dom-if" if="[[isMobile]]">
+                            <table>   
+                               <template is="dom-repeat" items="[[item.cells]]" as="cell">
+                                <tr>
+                                    <td style="width: 87px"><span>[[cell.label]]: </span></td>
+                                    <td>{{cell.value}}</td>
+                                </tr>
+                                                  
+                                </template>
+                            </table>
                         </template>
-                      </template>                     
-                  </ul>
+                        
+                        <template is="dom-if" if="[[!isMobile]]">
+                            <table> 
+                                <template is="dom-repeat" items="[[item.cells]]" as="cell">
+                                   <tr class="full-screen"> <td class="cell">[[cell]]</td> </tr> 
+                                </template>                
+                            </table>             
+                        </template>  
+                        
+                    
+                            
+                  </div>
               </div>
             </template>
        </div>
     `;
   }
 
+    static get properties() {
+        return {
+            headers: {
+                type: Array,
+                value: [],
+                observer: '_onPropsChange'
+            },
+            rows : {
+                type : Array,
+                value : []
+            },
+            grid: {
+                type: Array,
+                value: []
+            },
+            isMobile:{
+                type: Boolean,
+                value: false,
+                observer: '_redefineData'
+            }
+        };
+    }
+
   ready(){
-    super.ready();
-      const ro = new ResizeObserver((entries, observer) => {
-          for (const entry of entries) {
-              const width = entry.contentRect.width;
-              console.log(width);
-              if(width < 442){
-                  this.mode_movil = true;
-              } else {
-                  this.mode_movil = false;
-
-              }
-
-          }
-      });
-      ro.observe(this.$.container);
-
+     super.ready();
+     if(window.innerWidth <= 530){
+       this.set('isMobile', true);
+     }
+     this._resizeListener();
   }
-  static get properties() {
-    return {
-      headers: {
-        type: Array,
-        value: [],
-        observer: '_onPropsChange'
-      },
-        rows : {
-       type : Array,
-       value : []
-      },
-      grid: {
-          type: Array,
-          value: []
-      },
-      mode_movil:{
-        type: Boolean,
-        value: false,
-        observer: '_redefineData'
-      }
-    };
-  }
+
+
+    _resizeListener() {
+        window.addEventListener('resize', () => {
+            const width = window.innerWidth;
+            if (width <= 530) {
+                this.isMobile = true;
+                this._displayContent('none');
+            } else {
+                this.isMobile = false;
+                this._displayContent('block');
+            }
+        }, true);
+    }
+
+    _displayContent(dis) {
+        const rowList = this.shadowRoot.querySelectorAll('.row');
+        if(dis === 'none'){
+            this._removeLastActive();
+        }
+        for (const row of rowList) {
+            row.style.display = dis;
+        }
+    }
 
     displayNext(el){
-      const content = el.target.nextElementSibling;
-      if (content.style.display === "block") {
-          content.style.display = "none";
-      } else {
-          content.style.display = "block";
+      if(this.isMobile){
+          const content = el.target.nextElementSibling;
+          if (content.style.display === 'block') {
+              content.style.display = 'none';
+              el.target.innerText = el.target.innerText.replace('+','-');
+              el.target.classList.remove('row_active');
+          } else {
+              content.style.display = 'block';
+              el.target.innerText = el.target.innerText.replace('-','+');
+              this._removeLastActive();
+              el.target.classList.add('row_active');
+          }
       }
    }
+
+    _removeLastActive() {
+        const row = this.shadowRoot.querySelector('.row_active');
+        if(row){
+            row.innerText = row.innerText.replace('+','-');
+            row.classList.remove('row_active');
+            row.nextElementSibling.style.display = 'none';
+        }
+    }
 
     _onPropsChange(){
         this._redefineData();
@@ -123,7 +196,7 @@ class ResponsiveTable extends PolymerElement {
 
     _redefineData(){
         const newGrid = [];
-        if(this.mode_movil === false){
+        if(this.isMobile === false){
             for(const index in this.headers){
                 const column = {};
                 column.header = this.headers[index];
@@ -139,7 +212,7 @@ class ResponsiveTable extends PolymerElement {
 
             for(const row of this.rows){
                 const column = {};
-                column.header = row.title;
+                column.header = '- ' + row.title;
                 column.cells = [];
                 for(const index in this.headers){
                     const cell = {};
@@ -150,8 +223,7 @@ class ResponsiveTable extends PolymerElement {
                 newGrid.push(column);
             }
         }
-
-        this.grid = newGrid;
+        this.set('grid', newGrid);
     }
 
 }
